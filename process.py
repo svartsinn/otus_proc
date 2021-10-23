7c5e6a31550244d670f7c9dd1429b12db0f9059b
a.ostaenkov <a.ostaenkov@okko.tv>
2021-10-24 00:16:40 +0500

    Fix review comments

--- process.py
+++ process.py
@@ -7,13 +7,13 @@ import sys
 user_reg = r'^\S*'
 cpu_reg = r'\d*\s\s(\d*\.\d*)'
 mem_reg = r'\d*\s\s\d*\.\d*\s*(\d*\.\d*)'
-proc_reg = r'\/.*$'
+proc_reg = r'\s\s\s\d:\d\d\s(.*)$'
 
 proc = subprocess.Popen(('ps', 'aux'), stdout=subprocess.PIPE, universal_newlines=True)
 
 out, err = proc.communicate()
 
-out = out.split('\n')[1:]
+out = out.split('\n')[1:-1]
 proc_count = len(out)
 
 result_list = []
@@ -24,7 +24,7 @@ for line in out:
     cpu = re.search(cpu_reg, line)
     proc = re.search(proc_reg, line)
     if user is not None and mem is not None and cpu is not None and proc is not None:
-        result_list.append([proc.group(), user.group(), float(mem.groups()[0]), float(cpu.groups()[0])])
+        result_list.append([proc.groups()[0], user.group(), float(mem.groups()[0]), float(cpu.groups()[0])])
 
 user_list = []
 memory = 0
@@ -35,9 +35,9 @@ for elem in result_list:
     proc += elem[3]
 
 max_memory = max(result_list, key=lambda item: item[2])
-max_memory = max_memory[0].split('/')
+max_memory = max_memory[0]
 max_cpu = max(result_list, key=lambda item: item[3])
-max_cpu = max_cpu[0].split('/')
+max_cpu = max_cpu[0]
 
 users = ', '.join(set(user_list))
 user_process = Counter(user_list).most_common()
@@ -56,8 +56,8 @@ with open(filename, 'w') as f:
         print(elem[0] + ': ' + str(elem[1]))
     print(f'Всего памяти используется: {round(memory, 2)}%')
     print(f'Всего CPU используется: {round(proc, 2)}%')
-    print(f'Больше всего памяти использует: {(max_memory[-1])[:20]}')
-    print(f'Больше всего CPU использует: {(max_cpu[-1][:20])}')
+    print(f'Больше всего памяти использует: {max_memory[:20]}')
+    print(f'Больше всего CPU использует: {max_cpu[:20]}')
     sys.stdout = original_stdout
 
 with open(filename, 'r') as f:
@@ -65,5 +65,3 @@ with open(filename, 'r') as f:
 
 for line in lines:
     print(line, end='')
-
-
